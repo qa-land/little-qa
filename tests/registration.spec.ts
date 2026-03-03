76a162f2fe29b6f6d75ff451757371d1cc9f86e1
baba-ojo-jw <bojo@justworks.com>
2026-03-03 13:48:47 -0500

    kick

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -7,7 +7,7 @@ test("Registration", async ({ page }) => {
   await page.getByLabel("Country").selectOption("Canada");
   await page.getByLabel("Account Type").selectOption("Engineer");
   await page.getByLabel("Email").fill("qa_testers@qabrains.com");
-  await page.getByLabel("Password", { exact: true }).fill("Password566123");
-  await page.getByLabel("Confirm Password").fill("Password566123");
+  await page.getByLabel("Password", { exact: true }).fill("Password123");
+  await page.getByLabel("Confirm Password").fill("Password123");
   await page.getByRole("button", { name: "Sign Up" }).click();
 });
