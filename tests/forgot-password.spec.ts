9425941a3a1d5c8ffa40b04ee552bd5bcabc7a84
baba-ojo-jw <bojo@justworks.com>
2026-03-03 15:36:39 -0500

    kick

--- tests/forgot-password.spec.ts
+++ tests/forgot-password.spec.ts
@@ -4,5 +4,5 @@ test("Forgot Password", async ({ page }) => {
   await page.goto("https://practice.qabrains.com/forgot-password");
 
   await page.getByLabel("Email").fill("qa_testers@qabrains.com");
-  await page.getByRole("button", { name: "Reset Passwsahord" }).click();
+  await page.getByRole("button", { name: "Reset Password" }).click();
 });
